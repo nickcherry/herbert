a7635a93de8ef114e6bd9e4775d2e3b0229341b8
Nick Cherry <nickcherryjiggz@gmail.com>
2026-05-21 13:38:13 -0400

    Allow any method for ping

--- packages/server/src/server/routes/ping.ts
+++ packages/server/src/server/routes/ping.ts
@@ -2,17 +2,7 @@ import { jsonResponse } from "@herbert/server/server/jsonResponse";
 
 export const pingRoutePath = "/ping";
 
-export function handlePingRoute({ request }: { readonly request: Request }) {
-  if (request.method !== "GET") {
-    return jsonResponse(
-      {
-        ok: false,
-        error: "method_not_allowed",
-      },
-      { status: 405 },
-    );
-  }
-
+export function handlePingRoute() {
   return jsonResponse({
     ok: true,
     service: "herbert-server",
